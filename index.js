c714f36743720e6b22ab57f8a5a18486bbd219c9
24LP4621 24LP4621 <24LP4621@G301-CC0195.local>
2024-12-02 17:15:16 +0800

    Variable

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello world From JS Variable");
+// 1. "Comments can make code readable" - нэг мөртэй коммент болгох
+// 2. Variable.js нэртэй файл үүсгэх, number, string, boolean, undefined, null зэрэг утгуудыг өгөх
+// 3. Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн утгуудыг шалгах, number, string, boolean, undefined, null гэх мэт
+// 4. Утга оноогоогүй хувьсагч зарлах
+// 5. Утга оноосон хувьсагч зарлах
+// 6. Олон мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+// 7. Нэг мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+
+const firstName = "Amar";
+console.log(typeof firstName);
+
+const lastName = "Davaadalai";
+console.log(typeof lastName);
+
+let maritalStatus = "none";
+console.log(maritalStatus);
+
+let country = "Mongolia";
+console.log(country);
+
+let age = 24;
+console.log(age);
